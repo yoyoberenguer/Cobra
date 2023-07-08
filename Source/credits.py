ca0c43237fec1cd2a461aebef94bc5b2a99a0a8f
Yoann Berenguer <38462796+yoyoberenguer@users.noreply.github.com>
2023-07-08 22:13:33 +0100

    Delete Source directory

--- Source/credits.py
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-# SOUNDS
-# Attribution / Credit Required
-# Sound effects licensed via this standard license require attribution in the form of a link or credit.
-# You should credit us in the main credits for the project, from your project website, IMDB credits, Y
-# ouTube description etc. We are fairly flexible on how you do this, but we recommend something like:
-# “Sound effects obtained from https://www.zapsplat.com“
-# “Additional sound effects from https://www.zapsplat.com“
-# “Music from https://www.zapsplat.com“
